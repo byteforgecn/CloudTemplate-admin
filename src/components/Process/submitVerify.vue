d49fbb27388e710d356a7a1f73b8bb0817bbaa30
gssong <1742057357@qq.com>
2023-07-02 18:48:58 +0800

    add 增加审批意见

--- src/components/Process/submitVerify.vue
+++ src/components/Process/submitVerify.vue
@@ -1,6 +1,17 @@
 <template>
-  <el-dialog v-model="dialog.visible" v-loading="loading" :title="dialog.title" width="30%" draggable>
-    <span>It's a draggable Dialog</span>
+  <el-dialog v-model="dialog.visible" v-loading="loading" :title="dialog.title" width="50%" draggable :close-on-click-modal="false">
+    <el-form :model="form" label-width="120px">
+      <el-form-item label="消息提醒">
+        <el-checkbox-group v-model="form.messageType">
+          <el-checkbox label="1" name="type" disabled>站内信</el-checkbox>
+          <el-checkbox label="2" name="type">邮件</el-checkbox>
+          <el-checkbox label="3" name="type">短信</el-checkbox>
+        </el-checkbox-group>
+      </el-form-item>
+      <el-form-item label="审批意见">
+        <el-input v-model="form.message" type="textarea" resize="none" />
+      </el-form-item>
+    </el-form>
     <template #footer>
       <span class="dialog-footer">
         <el-button @click="dialog.visible = false">取消</el-button>
@@ -32,6 +43,13 @@ const dialog = reactive<DialogOption>({
   title: '提示'
 });
 
+const form = ref<Record<string, any>>({
+  taskId: undefined,
+  message: undefined,
+  variables: {},
+  messageType: ['1']
+});
+//打开弹窗
 const openDialog = (visible?: any) => {
   dialog.visible = visible;
 };
@@ -41,12 +59,10 @@ const emits = defineEmits(['submitCallback']);
 
 /** 办理流程 */
 const handleCompleteTask = async () => {
+  form.value.taskId = props.taskId;
   await proxy?.$modal.confirm('是否确认提交？');
-  let param = {
-    taskId: props.taskId
-  };
-  await completeTask(param).finally(() => (loading.value = false));
-  dialog.visible = false
+  await completeTask(form.value).finally(() => (loading.value = false));
+  dialog.visible = false;
   emits('submitCallback');
   proxy?.$modal.msgSuccess('操作成功');
 };
