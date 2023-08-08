936d23affc62eb57a8884d3caf32e690b2dcf890
gssong <1742057357@qq.com>
2023-08-08 22:40:37 +0800

    update 流程设计器中分配发起人变量错误，先移除

--- public/static/flowable-modeler/editor-app/configuration/properties-assignment-controller.js
+++ public/static/flowable-modeler/editor-app/configuration/properties-assignment-controller.js
@@ -64,7 +64,7 @@ angular.module('flowableModeler').controller('FlowableAssignmentPopupCtrl',
 
 
     $scope.assignmentOptions = [
-        {id: "initiator", title: $translate.instant('PROPERTY.ASSIGNMENT.IDM.DROPDOWN.INITIATOR')},
+        //{id: "initiator", title: $translate.instant('PROPERTY.ASSIGNMENT.IDM.DROPDOWN.INITIATOR')},
         {id: "user", title: $translate.instant('PROPERTY.ASSIGNMENT.IDM.DROPDOWN.USER')},
         {id: "users", title: $translate.instant('PROPERTY.ASSIGNMENT.IDM.DROPDOWN.USERS')},
         {id: "groups", title: $translate.instant('PROPERTY.ASSIGNMENT.IDM.DROPDOWN.GROUPS')}
